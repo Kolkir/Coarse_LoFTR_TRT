6c8bd7dec9e9b55a59c9fccf8903eed4628fe605
kolkir <rotate@ukr.net>
2022-01-16 15:16:45 +0200

    Update ONNX exporting accordint with a new model structure

--- export_onnx.py
+++ export_onnx.py
@@ -8,8 +8,12 @@ from utils import make_student_config
 
 def main():
     parser = argparse.ArgumentParser(description='LoFTR demo.')
-    parser.add_argument('--weights', type=str, default='weights/outdoor_ds.ckpt',
+    parser.add_argument('--out_file', type=str, default='weights/LoFTR_teacher.onnx',
+                        help='Path for the output ONNX model.')
+    parser.add_argument('--weights', type=str, default='weights/LoFTR_teacher.pt',  # weights/outdoor_ds.ckpt
                         help='Path to network weights.')
+    parser.add_argument('--original', action='store_true',
+                        help='If specified the original LoFTR model will be used.')
     parser.add_argument('--device', type=str, default='cuda',
                         help='cpu or cuda')
     parser.add_argument('--prune', default=False, help='Do unstructured pruning')
@@ -19,19 +23,27 @@ def main():
 
     device = torch.device(opt.device)
 
+    if opt.original:
+        model_cfg = default_cfg
+    else:
+        model_cfg = make_student_config(default_cfg)
+
     print('Loading pre-trained network...')
-    make_student_config(default_cfg)
-    model = LoFTR(config=default_cfg)
-    # checkpoint = torch.load(opt.weights)
-    # if checkpoint is not None:
-    #     missed_keys, unexpected_keys = model.load_state_dict(checkpoint['state_dict'], strict=False)
-    #     if len(missed_keys) > 0:
-    #         print('Checkpoint is broken')
-    #         return 1
-    #     print('Successfully loaded pre-trained weights.')
-    # else:
-    #     print('Failed to load checkpoint')
-    #     return 1
+    model = LoFTR(config=model_cfg)
+    checkpoint = torch.load(opt.weights)
+    if checkpoint is not None:
+        if 'state_dict' in checkpoint:
+            state_dict = checkpoint['state_dict']
+        else:
+            state_dict = checkpoint['model_state_dict']
+        missed_keys, unexpected_keys = model.load_state_dict(state_dict, strict=False)
+        if len(missed_keys) > 0:
+            print('Checkpoint is broken')
+            return 1
+        print('Successfully loaded pre-trained weights.')
+    else:
+        print('Failed to load checkpoint')
+        return 1
 
     if opt.prune:
         print('Model pruning')
@@ -61,7 +73,7 @@ def main():
 
     with torch.no_grad():
         dummy_image = torch.randn(1, 1, default_cfg['input_height'], default_cfg['input_width'], device=device)
-        torch.onnx.export(model, (dummy_image, dummy_image), 'loftr.onnx', verbose=True, opset_version=11)
+        torch.onnx.export(model, (dummy_image, dummy_image), opt.out_file, verbose=True, opset_version=11)
 
 
 if __name__ == "__main__":
